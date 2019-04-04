6cdfe8f56d08f12b53f698923c2292583501c3ef
Unknown <kiwol95@gmail.com>
2019-04-04 11:11:32 +0200

    Handle Bishop movements

--- src/main/java/fr/rphstudio/chess/game/pieces/Bishop.java
+++ src/main/java/fr/rphstudio/chess/game/pieces/Bishop.java
@@ -1,25 +1,74 @@
 package fr.rphstudio.chess.game.pieces;
 
 import fr.rphstudio.chess.game.IMove;
+import fr.rphstudio.chess.game.Piece;
 import fr.rphstudio.chess.game.Tray;
 import fr.rphstudio.chess.interf.IChess;
-import fr.rphstudio.chess.interf.OutOfBoardException;
+
 
 import java.util.ArrayList;
 
 public class Bishop implements IMove {
-    // fou
+
     @Override
     public ArrayList<IChess.ChessPosition> getPossibleMoves(IChess.ChessPosition pos, Tray tray) {
         ArrayList<IChess.ChessPosition> listPos = new ArrayList<>();
-        for(int i = 7; i > 0; i--) {
+        Piece[][] table = tray.getTable();
 
-                if (pos.x + i < IChess.BOARD_HEIGHT && pos.y + i < IChess.BOARD_WIDTH) listPos.add(new IChess.ChessPosition(pos.x + i, pos.y + i));
-                if (pos.x - i >= 0 && pos.y + i < IChess.BOARD_WIDTH) listPos.add(new IChess.ChessPosition(pos.x - i, pos.y + i));
+        for(int direction = 0; direction < 4; direction++) {
+            switch (direction) {
+                case 0 :
+                    for (int i = 1; i < 8; i++) {
+                        if (pos.x + i < IChess.BOARD_HEIGHT && pos.y + i < IChess.BOARD_WIDTH) {
+                            if (table[pos.x + i][pos.y + i] == null) {
+                                listPos.add(new IChess.ChessPosition(pos.x + i, pos.y + i));
+                            } else if (table[pos.x + i][pos.y + i].getChessColor() != tray.getPiece(pos).getChessColor()) {
+                                listPos.add(new IChess.ChessPosition(pos.x + i, pos.y + i));
+                                break;
+                            } else break;
+                        }
+                    }
+                    break;
 
-                if (pos.x + i < IChess.BOARD_HEIGHT && pos.y - i >= 0) listPos.add(new IChess.ChessPosition(pos.x + i, pos.y - i));
-                if (pos.x - i >= 0 && pos.y - i >= 0) listPos.add(new IChess.ChessPosition(pos.x - i, pos.y - i));
+                case 1 :
+                    for (int i = 1; i < 8; i++) {
+                        if (pos.x - i >= 0 && pos.y + i < IChess.BOARD_WIDTH) {
+                            if (table[pos.x - i][pos.y + i] == null) {
+                                listPos.add(new IChess.ChessPosition(pos.x - i, pos.y + i));
+                            } else if (table[pos.x - i][pos.y + i].getChessColor() != tray.getPiece(pos).getChessColor()) {
+                                listPos.add(new IChess.ChessPosition(pos.x - i, pos.y + i));
+                                break;
+                            } else break;
+                        }
+                    }
+                    break;
 
+                case 2 :
+                    for (int i = 1; i < 8; i++) {
+                        if (pos.x + i < IChess.BOARD_HEIGHT && pos.y - i >= 0) {
+                            if (table[pos.x + i][pos.y - i] == null) {
+                                listPos.add(new IChess.ChessPosition(pos.x + i, pos.y - i));
+                            } else if (table[pos.x + i][pos.y - i].getChessColor() != tray.getPiece(pos).getChessColor()) {
+                                listPos.add(new IChess.ChessPosition(pos.x + i, pos.y - i));
+                                break;
+                            } else break;
+                        }
+                    }
+                    break;
+
+                case 3 :
+                    for (int i = 1; i < 8; i++) {
+                        if (pos.x - i >= 0 && pos.y - i >= 0) {
+                            if (table[pos.x - i][pos.y - i] == null) {
+                                listPos.add(new IChess.ChessPosition(pos.x - i, pos.y - i));
+                            } else if (table[pos.x - i][pos.y - i].getChessColor() != tray.getPiece(pos).getChessColor()) {
+                                listPos.add(new IChess.ChessPosition(pos.x - i, pos.y - i));
+                                break;
+                            } else break;
+                        }
+                    }
+                    break;
+            }
         }
         return listPos;
     }
