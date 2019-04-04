505ca80d01d6328cb4286349379b411e48954aed
Unknown <yanis88357@gmail.com>
2019-04-04 17:44:41 +0200

    Correction bug pion
    (ça m'a l'air bon mais à tester dans tous les cas)

--- src/main/java/fr/rphstudio/chess/game/pieces/Pawn.java
+++ src/main/java/fr/rphstudio/chess/game/pieces/Pawn.java
@@ -21,53 +21,58 @@ public class Pawn implements IMove {
         Piece[][] table = tray.getTable();
         Piece pi = this.tray.getPiece(pos);
 
-        if (pi.getChessColor() == CLR_WHITE){
-                    if (pos.y == 6) {
-                        if ((table[pos.x][pos.y - 2] == null && table[pos.x][pos.y - 1] == null)) {
-                            listPos.add(new IChess.ChessPosition(pos.x, pos.y - 2));
-                        }
-                        if (table[pos.x][pos.y - 1] == null) {
-                            listPos.add(new IChess.ChessPosition(pos.x, pos.y - 1));
-                        }
-                    } else if (pos.y - 1 >= 0) {
+        if (pi.getChessColor() == CLR_WHITE) {
+                if (pos.y == 6) {
+                    if ((table[pos.x][pos.y - 2] == null && table[pos.x][pos.y - 1] == null)) {
+                        listPos.add(new IChess.ChessPosition(pos.x, pos.y - 2));
+                    }
+                    if (table[pos.x][pos.y - 1] == null) {
                         listPos.add(new IChess.ChessPosition(pos.x, pos.y - 1));
                     }
-
-            if (pos.x - 1 > 0) {
-                if (table[pos.x - 1][pos.y - 1] != null && table[pos.x - 1][pos.y - 1].getChessColor() != tray.getPiece(pos).getChessColor()) {
-                    listPos.add(new IChess.ChessPosition(pos.x - 1, pos.y - 1));
                 }
-            }
 
-            if (pos.x + 1 < IChess.BOARD_WIDTH && pos.y - 1 > 0) {
-                if (table[pos.x + 1][pos.y - 1] != null && table[pos.x + 1][pos.y - 1].getChessColor() != tray.getPiece(pos).getChessColor()) {
-                    listPos.add(new IChess.ChessPosition(pos.x + 1, pos.y - 1));
+                if (pos.y - 1 >= 0 && table[pos.x][pos.y - 1] == null) {
+                    listPos.add(new IChess.ChessPosition(pos.x, pos.y - 1));
                 }
-            }
 
-        } else if (pi.getChessColor() == CLR_BLACK){
-                    if (pos.y == 1) {
-                        if (table[pos.x][pos.y + 2] == null && table[pos.x][pos.y + 1] == null) {
-                            listPos.add(new IChess.ChessPosition(pos.x, pos.y + 2));
-                        }
-                        if (table[pos.x][pos.y + 1] == null) {
-                            listPos.add(new IChess.ChessPosition(pos.x, pos.y + 1));
-                        }
-                    } else if (pos.y + 1 < IChess.BOARD_HEIGHT) {
+                if (pos.x - 1 >= 0 && pos.y - 1 >= 0) {
+                    if (table[pos.x - 1][pos.y - 1] != null && table[pos.x - 1][pos.y - 1].getChessColor() != tray.getPiece(pos).getChessColor()) {
+                        listPos.add(new IChess.ChessPosition(pos.x - 1, pos.y - 1));
+                    }
+                }
+
+                if (pos.x + 1 < IChess.BOARD_WIDTH && pos.y - 1 >= 0) {
+                    if (table[pos.x + 1][pos.y - 1] != null && table[pos.x + 1][pos.y - 1].getChessColor() != tray.getPiece(pos).getChessColor()) {
+                        listPos.add(new IChess.ChessPosition(pos.x + 1, pos.y - 1));
+                    }
+                }
+
+        } else if (pi.getChessColor() == CLR_BLACK) {
+                if (pos.y == 1) {
+                    if (table[pos.x][pos.y + 2] == null && table[pos.x][pos.y + 1] == null) {
+                        listPos.add(new IChess.ChessPosition(pos.x, pos.y + 2));
+                    }
+                    if (table[pos.x][pos.y + 1] == null) {
                         listPos.add(new IChess.ChessPosition(pos.x, pos.y + 1));
                     }
+                }
+
+                if (pos.y + 1 < IChess.BOARD_HEIGHT && table[pos.x][pos.y + 1] == null) {
+                    listPos.add(new IChess.ChessPosition(pos.x, pos.y + 1));
+                }
 
 
-            if (pos.x - 1 > 0 && pos.y + 1 < IChess.BOARD_HEIGHT) {
-                if (table[pos.x - 1][pos.y + 1] != null && table[pos.x - 1][pos.y + 1].getChessColor() != tray.getPiece(pos).getChessColor()) {
-                    listPos.add(new IChess.ChessPosition(pos.x - 1, pos.y + 1));
+                if (pos.x - 1 >= 0 && pos.y + 1 < IChess.BOARD_HEIGHT) {
+                    if (table[pos.x - 1][pos.y + 1] != null && table[pos.x - 1][pos.y + 1].getChessColor() != tray.getPiece(pos).getChessColor()) {
+                        listPos.add(new IChess.ChessPosition(pos.x - 1, pos.y + 1));
+                    }
                 }
-            }
-            if (pos.x + 1 < IChess.BOARD_WIDTH && pos.y + 1 < 0) {
-                if (table[pos.x + 1][pos.y + 1] != null && table[pos.x + 1][pos.y + 1].getChessColor() != tray.getPiece(pos).getChessColor()) {
-                    listPos.add(new IChess.ChessPosition(pos.x + 1, pos.y + 1));
+                if (pos.x + 1 < IChess.BOARD_WIDTH && pos.y + 1 < IChess.BOARD_HEIGHT) {
+                    if (table[pos.x + 1][pos.y + 1] != null && table[pos.x + 1][pos.y + 1].getChessColor() != tray.getPiece(pos).getChessColor()) {
+                        listPos.add(new IChess.ChessPosition(pos.x + 1, pos.y + 1));
+                    }
                 }
-            }
+
         }
         return listPos;
     }
